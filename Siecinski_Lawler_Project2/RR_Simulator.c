c8e73a60a92047d7a8a77f5afbd33f6ec2177a47
SkiboUSN <49970625+SkiboUSN@users.noreply.github.com>
2020-04-03 00:19:43 -0400

    Update RR_Simulator.c

--- Siecinski_Lawler_Project2/RR_Simulator.c
+++ Siecinski_Lawler_Project2/RR_Simulator.c
@@ -1,154 +1,150 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
+#include <dirent.h>
+#include <string.h>
+#include <pthread.h>
+#include <semaphore.h>
 
-// structure to hold job information
-struct Job {
-    int runtime;        // variable to store how long it takes to complete job
-    int time_left;      // variable to store how much time is left on a job
-    bool complete;      // boolean variable to mark if job is complete for removal
-    int memory;         // variable to store how much memory the job takes up
-    bool allocated;     // boolean variable to tell wether or not job is allocated to page table
-    int start_time;     // variable to store start time
-    int time_complete;  // variable to store end time
+sem_t empty;
+pthread_mutex_t popmutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t writemutex = PTHREAD_MUTEX_INITIALIZER;
+
+//structs to support queue
+struct entry {
+    char *line;
+    struct entry *next;
+    struct entry *prev;
 };
 
+struct queue {
+    struct entry *queue[100];
+    struct entry head;
+    struct entry tail;
+};
 
-//return a random number between 0 and limit inclusive
-//used to ensure there is no skew in the random numbers generated
-int rand_lim(int limit) {
+struct queue myqueue;
 
-    int divisor = RAND_MAX/(limit+1);       
-    int retval;
-
-    do {
-        retval = rand() / divisor;
-    } while (retval > limit);
-
-    return retval;
+char* pop(){
+    pthread_mutex_lock(&popmutex);
+    struct entry tempEntry = *myqueue.tail.prev;
+    char *tempstr = tempEntry.line;
+    myqueue.tail.prev = tempEntry.prev;
+    tempEntry = *tempEntry.prev;
+    tempEntry.next = &myqueue.tail;
+    pthread_mutex_unlock(&popmutex);
+    return tempstr;
 }
 
-int main(int argc, char *argv[]){
-    system("export RANDOM_SEED");   //make RANDOM_SEED an environmental variable from shell variable
-    if(argc != 8){
-        printf("Usage: ./RR_Simulator {Memory Size} {Page Size} {Number of Jobs} {Minimum Runtime} {Maximum Runtime} {Minimum Job Memory} {Maximum Job Memory}\n"); //print usage for users
+void *consumer(void *tid){
+
+}
+
+int main(int argc, char *argv[]) {
+
+    /*pthread_t threads[(int)argv[2]];
+    int rc;
+    long t;
+    for(t=0; t<argv[2]; t++){
+       printf("In main: creating thread %ld\n", t);
+       rc = pthread_create(&threads[t], NULL, PrintHello, (void *)t);
+       if (rc){
+          printf("ERROR; return code from pthread_create() is %d\n", rc);
+          exit(-1);
+       }
+    }*/
+
+    //initialize queue
+    myqueue.head.next = &myqueue.tail;
+    myqueue.tail.prev = &myqueue.head;
+
+    if (strcmp(argv[1],"keyboard") == 0 || strcmp(argv[1],"k") == 0) //check if keyboard or file
+    {
+        char *buffer; //where the typed statement will be stored
+        size_t bufsize = 64; //how long the string the user can input
+        size_t characters;
+        printf("not here\n");
+        buffer = (char *)malloc(bufsize * sizeof(char));
+        //check if malloc failed
+            if( buffer == NULL)
+            {
+                perror("Unable to allocate buffer");
+                exit(1);
+            }
+        //how long the buffer currently is
+        struct entry tempEntry;
+        characters = getline(&buffer, &bufsize, stdin);
+        while(strcmp(buffer, "quit\n") != 0)
+        {
+
+            tempEntry.line = buffer;
+            tempEntry.next = myqueue.head.next;
+            tempEntry.prev = &myqueue.head;
+            struct entry secondTempEntry = *myqueue.head.next;
+            secondTempEntry.prev = &tempEntry;
+            myqueue.head.next = &tempEntry;
+
+
+            printf("Input: %s\n",buffer);
+            characters = getline(&buffer, &bufsize, stdin);
+            //sem_post(&empty);
+
+        }
+        free(buffer);
     }
-    else{
-
-        //set variables from parameters
-        char *ptr;
-        long mem_size = strtol(argv[1], &ptr, 10);
-        long page_size = strtol(argv[2], &ptr, 10);
-        long num_jobs = strtol(argv[3], &ptr, 10);
-        long min_run = strtol(argv[4], &ptr, 10);
-        long max_run = strtol(argv[5], &ptr, 10);
-        long min_mem = strtol(argv[6], &ptr, 10);
-        long max_mem = strtol(argv[7], &ptr, 10);
-        long seed = strtol(getenv("RANDOM_SEED"), &ptr, 10);
-        srand(seed);
-
-        //print simulator parameters
-        printf("Simulator Parameters:\n");
-        printf("Memory Size: %ld\nPage Size: %ld\nNumber of Jobs: %ld\nRuntime (min-max) timesteps: %ld-%ld\nMemory (min-max): %ld-%ld\nRandom Seed: %ld\n\n", mem_size,page_size,num_jobs,min_run,max_run,min_mem,max_mem,seed);
-
-        //set other needed variables
-        int num_complete = 0;
-        int unallocated = mem_size/page_size;
-        int page_table[unallocated];
-        int i;
-        int time_step = 1;
-        int curr_job = 0;
-        int k;
-        for(i = 0;i < unallocated;i++){
-            page_table[i] = 0;
+    else
+    {
+        FILE *fp;
+        printf("and here\n");
+        //make sure in the readme to note filetype must be specified and cannot be k or keyboard
+        fp = fopen(argv[1],"r");
+        printf("and 1\n");
+        if(fp == NULL){
+            printf("not a valid txt file\n");
+            exit(1);
         }
-
-        //make queue of jobs in form of array and fill in structure data
-        struct Job job_queue[num_jobs];
-        for(i = 0;i < num_jobs;i++){
-            job_queue[i].runtime = rand_lim(max_run-min_run)+min_run;
-            job_queue[i].memory = (rand_lim(max_mem-min_mem)+min_mem);
-            job_queue[i].time_left = job_queue[i].runtime;
-            job_queue[i].complete = false;
-            job_queue[i].allocated = false;
-        }
-
-        //print the job queue information
-        printf("Job Queue:\n\tJob #\tRuntime\tMemory\n");
-        for(i = 0;i < num_jobs;i++){
-            printf("\t%d\t%d\t%d\n",i+1,job_queue[i].runtime,job_queue[i].memory);
-        }
-
-        //start of timesteps for round robin scheduling
-        while(num_complete < num_jobs){
-            printf("Time Step %d:\n", time_step);
-
-            //allocate unallocated jobs
-            for(i = 0;i < num_jobs;i++){
-
-                if(job_queue[i].allocated == false && job_queue[i].complete == false && unallocated*page_size > job_queue[i].memory){
-                    int curr_mem = job_queue[i].memory;
-                    for(k = 0; k < mem_size/page_size; k++){
-                        if(page_table[k]==0){
-                            page_table[k]=i+1;
-                            unallocated--;
-                            curr_mem = curr_mem-page_size;
-                            if(curr_mem<=0){
-                                break;
-                            }
-                        }
-
-                    }
-                    job_queue[i].allocated = true;
-                    job_queue[i].start_time = time_step;
-                    printf("\tJob %d Starting\n", i+1);
-                }
+        char *buffer; //where the typed statement will be stored
+        size_t bufsize = 64; //how long the string the user can input
+        size_t characters;
+        buffer = (char *)malloc(bufsize * sizeof(char));
+        //check if malloc failed
+            if( buffer == NULL)
+            {
+                perror("Unable to allocate buffer");
+                exit(1);
             }
+        printf("and 2\n");
+        //how long the buffer currently is
+        int i = 0;
+        while((characters = getline(&buffer, &bufsize, fp)) != -1)
+        {
 
-            //progress current job
-            job_queue[curr_job].time_left--;
-            printf("\tContinuing Job %d\n", curr_job+1);
-            if(job_queue[curr_job].time_left==0){
-                job_queue[curr_job].complete = true;
-                job_queue[curr_job].allocated = false;
-                for(k = 0; k < mem_size/page_size; k++){
-                    if(page_table[k]==curr_job+1){
-                        page_table[k] = 0;
-                        unallocated++;
-                    }
-                }
-                job_queue[curr_job].time_complete = time_step;
-                num_complete++;
-                printf("\tJob %d Complete\n", curr_job+1);
-            }
+            struct entry *tempEntry;
+        printf("and 3\n");
+            tempEntry = myqueue.queue[i];
+        printf("and 4\n");
+            tempEntry->line = (char *)malloc(bufsize*sizeof(char));
+        printf("and 6\n");
+            strcpy(tempEntry->line, buffer);
+        printf("and 5\n");
+            tempEntry->next = myqueue.head.next;
+            tempEntry->prev = &myqueue.head;
+            struct entry *secondTempEntry = myqueue.head.next;
+            secondTempEntry->prev = tempEntry;
+            myqueue.head.next = tempEntry;
 
-            //find next job
-            for(i = (curr_job+1)%num_jobs; i < num_jobs; i=(i+1)%num_jobs){
-                if(job_queue[i].allocated == true && job_queue[i].complete == false){
-                    curr_job = i;
-                    break;
-                }
-                if(num_complete==num_jobs){break;}
-            }
 
-            //print page table
-            printf("\tPage Table:\n\t\t");
-            for(i = 0; i < mem_size/page_size; i++){
-                if(i%24 == 0){printf("\n\t\t");}
-                else if(i%4 == 0){printf(" ");}
-                printf("%d ", page_table[i]);
-            }
-            printf("\n");
-
-            time_step++;
+            printf("Input: '%s'\n",buffer);
+            i++;
+            //sem_post(&empty);
 
         }
-        //print completion stats
-        printf("\nJob Information:\n");
-        printf("\tJob #\tStart Time\tEnd Time\n");
-        for(i=0; i<num_jobs; i++){
-            printf("\t%d\t%d\t\t%d\n", i+1, job_queue[i].start_time, job_queue[i].time_complete);
+        struct entry tempEntry;
+        for(int i=0;i<10;i++){
+            tempEntry = *myqueue.head.next;
+            printf("%d: %s",i,tempEntry.line);
         }
+        free(buffer);
+        //closedir(fp);
+
     }
-    return 0;
 }
